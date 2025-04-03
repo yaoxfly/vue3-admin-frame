246428ea44110d3a91e6a21b1bfe8dec46a995d2
yaoxfly <1315892088@qq.com>
2025-04-03 15:45:59 +0800

    config: 路由添加404页面配置

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,18 +4,41 @@ const routes = [
     path: '/',
     name: 'home',
     component: Layout,
+    redirect: '/demo',
     children: [
       {
         path: '/demo',
         name: 'demo',
-        component: () => import('@/views/test/father')
+        component: () => import('@/views/test/father'),
+        meta: {
+          breadcrumb: [
+            {
+              title: 'home',
+              path: '/'
+            },
+            {
+              title: 'demo'
+            }
+          ]
+        }
 
       },
       // 如果是 404 只针对 /demo 的页面，放到 children 最后
       {
         path: ':pathMatch(.*)*',
         name: 'demo-404',
-        component: () => import('@/views/404/index')
+        component: () => import('@/views/404/index'),
+        meta: {
+          breadcrumb: [
+            {
+              title: '异常页面'
+
+            },
+            {
+              title: '404'
+            }
+          ]
+        }
       }
     ]
   },
@@ -23,7 +46,18 @@ const routes = [
   {
     path: '/:pathMatch(.*)*',
     name: '404',
-    component: () => import('@/views/404/index')
+    component: () => import('@/views/404/index'),
+    meta: {
+      breadcrumb: [
+        {
+          title: '异常页面'
+
+        },
+        {
+          title: '404'
+        }
+      ]
+    }
   }
 ]
 export default routes
