30a143eba6df7ffc194af80b732c01dbd2806100
yaoxfly <1315892088@qq.com>
2025-08-14 10:07:15 +0800

    refactor: 代码重构，删除tailwind相关的无用代码

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,9 @@ import legacy from '@vitejs/plugin-legacy'
 import AutoImport from 'unplugin-auto-import/vite'
 import postcssImport from 'postcss-import'
 import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
-// import progress from 'vite-plugin-progress'
 import browserslist from 'browserslist' // 统一js/css兼容性
 import checker from 'vite-plugin-checker'
 import externalGlobals from 'rollup-plugin-external-globals'
-import postcsspxtoviewport8plugin from 'postcss-px-to-viewport-8-plugin'
 const browserslistConfig = browserslist.loadConfig({ path: '.' }) // npx browserslist "> 0.04%, last 2 versions,Firefox ESR,not dead" 查询兼容的浏览器
 const externalGlobalsConfig = {
   vue: 'Vue',
@@ -74,25 +71,7 @@ const cdn = {
   ]
 }
 
-const postCssPxToViewportConfig = {
-  unitToConvert: 'px',
-  viewportWidth: 1920,
-  unitPrecision: 5, // 单位转换后保留的精度
-  propList: ['*'], // 能转化为vw的属性列表
-  viewportUnit: 'vw', // 希望使用的视口单位
-  fontViewportUnit: 'vw', // 字体使用的视口单位
-  selectorBlackList: [], // 需要忽略的CSS选择器，不会转为视口单位，使用原有的px等单位。
-  minPixelValue: 1, // 设置最小的转换数值，如果为1的话，只有大于1的值会被转换
-  mediaQuery: true, // 媒体查询里的单位是否需要转换单位
-  replace: true, //  是否直接更换属性值，而不添加备用属性
-  // exclude: [/node_modules\/element-plus/], // 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
-  include: [], // 如果设置了include，那将只有匹配到的文件才会被转换
-  landscape: false, // 是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
-  landscapeUnit: 'vw', // 横屏时使用的单位
-  landscapeWidth: 1024 // 横屏时使用的视口宽度
-}
-
-export default ({ mode, command }: ConfigEnv): UserConfigExport => defineConfig({
+export default ({ mode }: ConfigEnv): UserConfigExport => defineConfig({
   // 部署在二级目录下，也需要加个二级目录
   base: loadEnv(mode, process.cwd()).VITE_APP_PUBLIC_PATH,
   plugins: [
@@ -164,7 +143,6 @@ export default ({ mode, command }: ConfigEnv): UserConfigExport => defineConfig(
       watchFiles: true,
       enable: loadEnv(mode, process.cwd()).VITE_APP_CURRENT_MODE === 'development'
     }),
-    // progress(), //打包进度条，会覆盖掉打包详情信息，暂时不用了
     checker({
       typescript: true // 检查ts类型
     }),
@@ -189,10 +167,7 @@ export default ({ mode, command }: ConfigEnv): UserConfigExport => defineConfig(
     postcss: {
       plugins: [
         postcssImport,
-        autoprefixer,
-        // postcsspxtoviewport8plugin(postCssPxToViewportConfig),
-        tailwindcss
-
+        autoprefixer
       ]
     }
 
